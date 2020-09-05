f8db7c48c73f87bb7c0fca705d35159a014b6d99
Thatcooldevguy <tsommers150@gmail.com>
2020-09-05 13:34:37 -0400

    src

---
+++ Assets/Assets/menu/MenuScripts/ExitGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+// Update is called once per frame
+void Update()
+    {
+        System.Threading.Thread.Sleep(2000); 
+        quit();
+    }
+}
